0e70161693d0124fbb81e0af2f19b60c2f85b4b7
Alfaraz alam <alfarazalam2002@gmail.com>
2025-11-06 00:22:06 +0530

    Updated signup interactivity and fixed styles

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "https://swap-slot.onrender.com" });
+const API = axios.create({ baseURL: "https://swap-slot.onrender.com/api" });
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
